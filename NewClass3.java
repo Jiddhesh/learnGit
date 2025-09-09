f4013467c8f34644114a65c9ec65ae24a328aa70
jiddhesh shinde <jiddheshshinde@gmail.com>
2025-09-09 17:28:42 +0530

    new class added

---
+++ NewClass3.java
@@ -0,0 +1,8 @@
+import com.java.util.*;
+
+public class NewClass3 {
+	public static void main(String[] args) throws IOException {
+		System.out.println("This is java code for testing git.....");
+		System.out.println("concating two numbers: "+1+2);
+	}
+}
