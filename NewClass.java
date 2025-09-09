b41a814b8d4c0dea65c73e0f9ead2c892a8d7c48
jiddhesh shinde <jiddheshshinde@gmail.com>
2025-09-09 17:18:04 +0530

    updated NewClass.java

--- NewClass.java
+++ NewClass.java
@@ -2,6 +2,7 @@ import com.java.util.*;
 
 public class NewClass {
 	public static void main(String[] args) throws IOException {
-		System.out.println("This is java code for testing git.....")
+		System.out.println("This is java code for testing git.....");
+		System.out.println("concating two numbers: "+1+2);
 	}
 }
