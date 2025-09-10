19b4b68318ee5e0f0d0b85d9ed1a41a3f824094d
jiddhesh shinde <jiddheshshinde@gmail.com>
2025-09-10 12:52:05 +0530

    added new line

--- NewClass.java
+++ NewClass.java
@@ -4,5 +4,6 @@ public class NewClass {
 	public static void main(String[] args) throws IOException {
 		System.out.println("This is java code for testing git.....");
 		System.out.println("concating two numbers: "+1+2);
+		System.out.println();
 	}
 }
